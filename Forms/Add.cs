f84c32c20d2336b66505e343254debaff9e3a6c4
Danila <Danila@DESKTOP-ECLVNJF>
2023-05-10 18:18:14 +0300

    Добавьте файлы проекта.

---
+++ Forms/Add.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Add : Form
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+        public Book GetNewBook(object textBox3)
+        {
+            if(!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox3.text))
+            {
+                return new Book(textBox1.Text, textBox3.Text);
+            }
+            return null;
+
+        }
+
+        internal Book GetNewBook()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forms/Print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Forms;
+
+namespace Forms
+{
+    public partial class Print : Form
+    {
+        public Print()
+        {
+            InitializeComponent();
+        }
+        public void AddBook(Book book)
+        {
+            listBox1.Items.Add(book.ToString());
+        }
+    }
+}
